5dee7046a2c6139f35d73ba3fa8935b9585e8aa8
Rushan <rushanbuyorb@gmail.com>
2024-10-02 23:12:00 +0545

    added more pages and improved header

---
+++ frontend/src/components/RootLayout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="mx-40">
+        This is the footer section
+    </footer>
+  )
+};
+
+export { Footer };
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return "This is the home page";
+};
+
+export { Home };
